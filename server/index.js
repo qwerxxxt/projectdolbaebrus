66a01911043c9d94b5368e33d3876c4484248411
Fill <filimonpas@gmail.com>
2024-02-02 23:49:49 +0300

    server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,24 @@
-//23e24
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const sequelize = require('./db')
+
+const PORT = process.env.PORT
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+
+
+const start = async () => {
+    await sequelize.authenticate()
+    await sequelize.sync()
+
+    app.listen(PORT, () => {
+        console.log(`Server work on ${PORT}`)
+    })
+
+}
+
+start()
