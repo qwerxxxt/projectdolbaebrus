142334fe37ee52659e64dfebe350dcd267d2b661
SweeTMikRo <azazvevip6@gmail.com>
2024-02-03 00:49:46 +0300

    server

--- server/index.js
+++ server/index.js
@@ -3,13 +3,19 @@ const express = require('express')
 const cors = require('cors')
 const sequelize = require('./db')
 const models = require('./models/models')
+const {router} = require("express/lib/application");
+const path = require('path')
+const fileUpload = require('express-fileupload')
 
 const PORT = process.env.PORT
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.resolve(__dirname, 'static')))
+app.use(fileUpload({}))
 
+app.use('/api', router)
 
 
 const start = async () => {
