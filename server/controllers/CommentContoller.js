c2d7c00635339c69d49f3d5373581d5f2c4e1973
Fill <filimonpas@gmail.com>
2024-02-05 23:47:18 +0300

    server

--- server/controllers/CommentContoller.js
+++ server/controllers/CommentContoller.js
@@ -14,9 +14,9 @@ class CommentContoller{
     }
     async update(req,res,next){
         try{
-            const {text} = req.body
+            const {text_comment} = req.body
             const {id}=req.params
-            const update_comment = Comment.update({text}, {where: {id}})
+            const update_comment = Comment.update({text_comment}, {where: {id}})
             return res.json(update_comment)
         }catch (e) {
             next(ApiError.badRequest(e.message))
@@ -25,7 +25,7 @@ class CommentContoller{
     }
     async deleteComment(req,res){
         const id = req.params.id
-        const detcom = await Project.destroy({where: {id}})
+        const detcom = await Comment.destroy({where: {id}})
         return res.json(detcom)
     }
 }
