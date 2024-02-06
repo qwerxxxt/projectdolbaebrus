fb306075b3f928836ae004ef4814f3788ef82c97
I love treesome <stelatnikov96@gmail.com>
2024-02-06 20:32:11 +0300

    Маршрутизация +1

--- 123/src/components/AppRouter.js
+++ 123/src/components/AppRouter.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import {Route, Routes} from "react-router-dom";
+import React, {useContext} from 'react';
+import {Navigate, Route, Routes} from "react-router-dom";
 import {authRoutes, publicRoutes} from "../routes";
+import {HOME_ROUTE} from "../utils/consts";
+import {Context} from "../index";
 
 const AppRouter = () => {
-    const isAuth = true
+    const {user} = useContext(Context)
+    console.log(user)
     return (
         <Routes>
-            {isAuth && authRoutes.map(({path, element}) =>
+            {user.isAuth && authRoutes.map(({path, element}) =>
                 <Route path={path} element={element} exact/>
             )}
             {publicRoutes.map (({path,element}) =>
                 <Route key={path} path={path} element={element} exact/>
             )}
+            <Route path='*' element={<Navigate to={HOME_ROUTE}/>}/>
         </Routes>
     );
 };
--- 123/src/index.js
+++ 123/src/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, {createContext} from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import UserCard from "./card/UserCard";
 
+
+ export const Context = createContext(null)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Context.Provider value={{
+        user: new UserCard()
+    }}>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+
+    </Context.Provider>
 );
 
